152fcf998267ced10b279b385e28bcaeff6ac984
Sudhan <86077071+Sudhan-Dahake@users.noreply.github.com>
2024-10-28 19:41:14 -0400

    fixed pylint error in test_home_route_sierra

--- test_home_route_sierra.py
+++ test_home_route_sierra.py
@@ -1,24 +1,15 @@
 """This module will perform a basic test on the home route of the app"""
-import pytest
 from app import app as flask_app
 
-# Arrange
-@pytest.fixture
-def create_test_app():
-    """creates a test environment of the app"""
-    flask_app.config.update({'TESTING':True})
-    return flask_app
 
-# Arrange
-@pytest.fixture
-def create_test_client(create_test_app):
-    """creates a test client"""
-    return create_test_app.test_client()
+def test_home_route():
+    """Tests the home route of the application to ensure that it functions as expected"""
 
-def test_home_route(create_test_client):
-    """Tests the home route of the application to ensure that it functions as expected""" 
+    # Set up the test environment by configuring the app and creating a test client
+    flask_app.config.update({'TESTING': True})
+    client = flask_app.test_client()
     # Act
-    actual_response = create_test_client.get('/') # Call the home route for the app
+    actual_response = client.get('/')  # Call the home route for the app
     # Assert
     assert b"A webapp to detect a digit using hand sign language." in actual_response.data
     # check that the correct sub heading is on the page (b represents bytes)
